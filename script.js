c485addcfc5c876092f7b78dde86fd87f34209fa
Sreeja0202 <106142975+Sreeja0202@users.noreply.github.com>
2022-06-25 22:19:21 +0530

    as on 25th june

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-function validate()
-{
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    if(email.value == "" || password.value == "")
+// login form validation strts
+
+function loginvalidate()
+{  
+    
+    let email = document.getElementById("email");
+let password = document.getElementById("password");
+// let regexp = /^([a-zA-Z0-9\._\-]+)@([a-zA-Z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+let regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(regexp.test(email.value))
     {
-        return false;
+        return true;
     }
     else
     {
-        return true;
+        alert("Please esure your emailid is correct");
+        return false;
+    }
+}
+
+// login form validation ends
+
+// regitration form validation starts
+
+function registervalidate()
+{
+    let name = document.getElementById('name').value;
+    let regname =/^[A-Za-z\. ]{3,30}$/;
+    let dob = document.getElementById("dob").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let num = document.getElementById("num").value;
+    let regnum = /^\d{3}[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    function registervalidate()
+    {
+        if(regname.test(name.value))
+        {
+            alert("Name format is correct");
+            return true;
+        }
+        else
+        {
+            alert("Name format is wrong");
+            return false;
+        }
     }
 }
